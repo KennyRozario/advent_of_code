bf7ecb1b7e9c41687d7ff20e5b3024d3de181823
Kenny Rozario <kenny.rozario@uwaterloo.ca>
2020-12-01 19:56:03 -0500

    added solution for part 2 and updated p1 sol 2

--- src/day1/report_repair.py
+++ src/day1/report_repair.py
@@ -7,7 +7,7 @@ def get_expenses_from_report():
     return expenses
 
 
-def repair_expense_report_s1():
+def repair_expense_report_s1(expected_value):
     expenses = get_expenses_from_report()
     expenses.sort()
 
@@ -18,9 +18,9 @@ def repair_expense_report_s1():
         num2 = expenses[j]
         total = num1 + num2
 
-        if total == 2020:
+        if total == expected_value:
             return num1 * num2
-        elif total < 2020:
+        elif total < expected_value:
             i += 1
         else:
             j -= 1
@@ -28,22 +28,43 @@ def repair_expense_report_s1():
     return -1
 
 
-def repair_expense_report_s2():
-    expenses = get_expenses_from_report()
+def repair_expense_report_s2(expected_value, provided_expenses=None):
+    if not provided_expenses:
+        expenses = get_expenses_from_report()
+    else:
+        expenses = provided_expenses
     expense_pairings = dict()
 
     for expense in expenses:
         try:
             other_expense = expense_pairings[expense]
         except KeyError:
-            expense_pairings[2020 - expense] = expense
+            expense_pairings[expected_value - expense] = expense
         else:
             return expense * other_expense
 
+    return -1
+
+
+def repair_expense_report_part2(expected_value):
+    expenses = get_expenses_from_report()
+    expenses_set = set(expenses)
+
+    for expense in expenses:
+        expenses_set.remove(expense)
+        result = repair_expense_report_s2(expected_value - expense, expenses_set)
+
+        if result == -1:
+            expenses_set.add(expense)
+            continue
+
+        return result * expense
+
 
 def main():
-    print(repair_expense_report_s1())
-    print(repair_expense_report_s2())
+    print(repair_expense_report_s1(2020))
+    print(repair_expense_report_s2(2020))
+    print(repair_expense_report_part2(2020))
 
 
 if __name__ == "__main__":
--- tests/day1/test_report_repair.py
+++ tests/day1/test_report_repair.py
@@ -10,10 +10,15 @@ def mock_get_expenses_from_report(mocker):
 
 
 def test_solution1_succeeds_against_sample(mock_get_expenses_from_report):
-    result = report_repair.repair_expense_report_s1()
+    result = report_repair.repair_expense_report_s1(2020)
     assert result == 514579
 
 
 def test_solution2_succeeds_against_sample(mock_get_expenses_from_report):
-    result = report_repair.repair_expense_report_s2()
+    result = report_repair.repair_expense_report_s2(2020)
     assert result == 514579
+
+
+def test_part2_solution_succeeds_against_sample(mock_get_expenses_from_report):
+    result = report_repair.repair_expense_report_part2(2020)
+    assert result == 241861950
